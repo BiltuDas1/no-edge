2183fa5f82f614b7dd5d6d6cb9887603caa60f3d
Biltu Das <billionto@gmail.com>
2023-01-19 23:50:13 +0530

    added more parameters according to issue #5

--- msedge/msedge.cpp
+++ msedge/msedge.cpp
@@ -1,5 +1,4 @@
 #include"../include/compn.h"
-WinVisible w = false;
 
 class startup2
 {
@@ -16,14 +15,6 @@ public:
 			exit(1);
 		}
 
-		// Desktop Location
-		system("reg query \"HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" /v Desktop | more +2>%tmp%\\registry");
-		file.open(tmp + "\\\\registry");
-		getline(file, desktop);
-		file.close();
-		delf(tmp + "\\\\registry");
-		desktop.erase(0, 32);
-
 		// Current Directory
 		cd = fs::current_path().string();
 
@@ -33,22 +24,6 @@ public:
 		if (!filef(noedgeconf + "\\\\msedge.ini"))
 		{
 			failm("Error: NO Edge configuration file isn't found. Program Terminated.", "NoEdge Error", MB_ICONERROR);
-			exit(1);
-		}
-
-		// EDGE Directory
-		system("reg query \"HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\msedge.exe\" /ve | more +2>%tmp%\\registry");
-		file.open(tmp + "\\\\registry");
-		getline(file, edge);
-		file.close();
-		delf(tmp + "\\\\registry");
-		edge.erase(0, 27);
-		temp_int = (int)edge.find_last_of("\\");
-		edge.erase(temp_int);
-		replace_all(edge, "\\", "\\\\");
-		if (!fs::is_directory(edge))
-		{
-			failm("Error: MS Edge is not installed. Program Terminated.", "NoEdge Error", MB_ICONERROR);
 			exit(1);
 		}
 
@@ -88,74 +63,98 @@ protected:
 
 	void Default(string str)
 	{
+		// Remove all after &form
 		if (grep(str, "&form="))
 			str.erase(str.find("&form="));
 		if (grep(str, "https://www.bing.com/search?q="))
 		{
+			// Deleting bing from url
 			replace_all(str, "https://www.bing.com/search?q=", "");
+			if (grep(str, "&filters"))
+			{
+				str.erase(0, str.find("&filters") + 8);
+				str.erase(0, str.find("%22") + 3);
+				str.erase(str.find("%22"));
+			}
 			replace_all(se, "$query$", str);
 			str = se;
 		}
-		system(("explorer.exe \"" + str + "\"").c_str());
+		ShellExecuteA(NULL, "open", "rundll32.exe", ("url, OpenURL " + str).c_str(), NULL, SW_SHOW);
 	}
-
-	void Redirect(string str)
+	// Function for opening direct links
+	void Direct(string str)
 	{
-		system(("explorer.exe \"" + str +"\"").c_str());
+		ShellExecuteA(NULL, "open", "rundll32.exe", ("url, OpenURL " + str).c_str(), NULL, SW_SHOW);
 	}
 
 	void Video(string str)
 	{
 		str.erase(0, str.find("&url=")+5);
-		system(("explorer.exe \"" + urlDecode(str) +"\"").c_str());
+		ShellExecuteA(NULL, "open", "rundll32.exe", ("url, OpenURL " + urlDecode(str)).c_str(), NULL, SW_SHOW);
 	}
 
 public:
-	func(int* argc, char** argv)
+	func(LPWSTR lpCmdLine)
 	{
 		// No Parameters
-		if(*argc == 1)
-			system("explorer.exe \"https://github.com/BiltuDas1\"");
+		if (lpCmdLine == NULL || lpCmdLine[0] == 0)
+			ShellExecuteA(NULL, "open", "rundll32.exe", "url, OpenURL https://github.com/BiltuDas1", NULL, SW_SHOW);
+		// Private Browsing
+		else if (grep(lpw_s(lpCmdLine), "--inprivate"))
+			failm("Private Window feature is not available into this version, Check Official repo for more information", "Information", MB_ICONINFORMATION);
 		// Default Browser Parameter
-		else if (grep(argv[1], "--default-search-provider"))
+		else if (grep(lpw_s(lpCmdLine), "--default-search-provider"))
 			exit(0);
-		// Redirecting link to default browser
-		else if (grep(argv[1], "--edge-redirect"))
+		// Startup Window Parameter
+		else if (grep(lpw_s(lpCmdLine), "--no-startup-window"))
+			exit(0);
+		// Single Argument
+		else if (grep(lpw_s(lpCmdLine), "--single-argument"))
 		{
-			temp_str = argv[2];
+			temp_str = lpw_s(lpCmdLine);
+			temp_str.erase(0, temp_str.find(" ") + 1);
+			Direct(temp_str);
+		}
+		// Windows Jumplist
+		else if (grep(lpw_s(lpCmdLine), "--win-jumplist-action"))
+		{
+			temp_str = lpw_s(lpCmdLine);
+			temp_str.erase(0, temp_str.find(" ") + 1);
+			Direct(temp_str);
+		}
+		// Redirecting link to default browser
+		else if (grep(lpw_s(lpCmdLine), "--edge-redirect"))
+		{
+			temp_str = lpw_s(lpCmdLine);
 			// Decoding Url
 			if (grep(temp_str, "microsoft-edge:?url="))
 			{
-				temp_str.erase(0, 20);
+				temp_str.erase(0, temp_str.find(" ") + 21);
 				temp_str = urlDecode(temp_str);
 			}
 			else
 			{
-				temp_str.erase(0, 15);
+				temp_str.erase(0, temp_str.find(" ") + 16);
 			}
 
 			if (grep(temp_str, "https://www.bing.com/WS/redirect/"))
-				Redirect(temp_str);
+				Direct(temp_str);
 			else if (grep(temp_str, "Microsoft.Windows.Cortana_cw5n1h2txyewy"))
 				Video(temp_str);
 			else
 				Default(temp_str);
 		}
 		else
-		{
-			w = WinVisible(true);
-			failm("Error: Unknown Parameter","Error", MB_ICONERROR);
-			for (int i = 0; i < *argc; i++)
-				cout << argv[i] << endl;
-			lkey = _getch();
-		}
+			failm("Error: Unknown Parameter\n" + lpw_s(lpCmdLine), "Error", MB_ICONERROR);
 	}
 };
 
-int main(int argc, char** argv)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-	system("title Microsoft Edge debug screen");
 	startup2 start;
-	func f(&argc, argv);
+	func f(lpCmdLine);
 	return 0;
 }
